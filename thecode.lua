cf1458d1542544d32b023a9ed6b8e0696a665d53
Noëlle <noelle.d.anthony@gmail.com>
2020-04-05 18:16:35 -0400

    Add basic movement engine

--- thecode.lua
+++ thecode.lua
@@ -4,6 +4,13 @@
 playing = false
 music_going = false
 
+SPRITESIZE = 16
+
+LEFT  = {-1,0}
+RIGHT = {1,0}
+UP    = {0,-1}
+DOWN  = {0,1}
+
 pl = {}
 pl.moving = false
 pl.x = 0
@@ -15,8 +22,27 @@ pl.dir = 0
 pl.width = 8
 pl.height = 8
 
-function move()
-    pl.moving = true
+function iswalkable(x, y)
+    s = mget(x,y)
+    if fget(s, 0) then -- the first flag set on a sprite determines whether it's walkable
+        return true
+    end
+    return false
+end
+
+-- ent is the entity that's moving
+-- dctn is one of LEFT, RIGHT, UP, or DOWN
+function move(ent, dctn) 
+    if ent.moving == false then
+        tarx = ent.x + dctn[0]*SPRITESIZE
+        tary = ent.y + dctn[1]*SPRITESIZE
+        if iswalkable(tarx, tary) then
+            ent.moving = true
+            ent.tarx = tarx
+            ent.tary = tary
+            ent.movedctn = dctn
+        end
+    end
     -- do things here
 end
 
@@ -48,6 +74,18 @@ end
 
 function _update()
     -- run every frame
+    if pl.moving then
+        pl.x = pl.x + pl.movedctn[0]
+        pl.y = pl.y + pl.movedctn[1]
+        if pl.x == pl.tarx and pl.y == pl.tary then
+            pl.moving = false
+        end
+    else
+        if btn(0) then move(pl, LEFT) end
+        if btn(1) then move(pl, RIGHT) end
+        if btn(2) then move(pl, UP) end
+        if btn(3) then move(pl, DOWN) end
+    end
 end
 
 function _draw()
